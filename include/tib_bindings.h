3a9ecbd37cfa3ca03a1054140feec200c874d57b
Chris Antos <sparrowhawk996@gmail.com>
2026-08-02 10:56:45 -0700

    Dispatcher implementation without a trie.
    Will do performance testing to determine whether a trie is really
    necessary.  I expect a trie is overkill on modern hardware, and the
    complexity of a trie implementation is unfavorable, so hopefully that
    won't be needed.

--- include/tib_bindings.h
+++ include/tib_bindings.h
@@ -7,6 +7,7 @@
 
 #include "tib_base.h"
 
+#include <memory>
 #include <vector>
 #include <assert.h>
 
@@ -23,7 +24,10 @@ enum class binding_type : uint8_t
 
 typedef int32_t (*bindable_func_t)(tib::editor_context& ctx, const char* name);
 
+#ifdef USE_TRIE
 struct key_trie;
+#endif
+
 class key_table;
 
 class binding_target
@@ -74,8 +78,10 @@ struct key_binding
     binding_target      target;
 };
 
-class key_table
+class key_table : public std::enable_shared_from_this<key_table>
 {
+    friend class dispatcher;
+
 public:
                         ~key_table();
                         key_table() = default;
@@ -84,15 +90,28 @@ public:
     void                remove(const cstring& sequence);
     void                clear();
 
+    // TODO:  Enumerate m_bindings to be able to report current available key
+    // bindings.
+    // TODO:  Optionally filter the enumeration to only key bindings that
+    // match a prefix sequence.
+
+#ifdef USE_TRIE
     const key_trie*     get_trie();
+#endif
 
 private:
+#ifdef USE_TRIE
     void                free_trie();
     void                free_trie(key_trie* node);
+#endif
 
     std::vector<key_binding> m_bindings;
+#ifdef USE_TRIE
     key_trie*           m_trie = nullptr;
+#endif
 };
+
+typedef std::vector<std::shared_ptr<key_table>> key_table_list;
 
 enum dispatch_outcome { miss, more, match };
 
@@ -102,13 +121,24 @@ public:
                         ~dispatcher() = default;
                         dispatcher() = default;
 
+    void                init(const key_table_list& tables);
+
     void                reset();
     dispatch_outcome    step(char c);
 
+    const cstring&      get_sequence() const { return m_sequence; }
+    const binding_target* get_target() const { return m_target; }
+    dispatch_outcome    get_outcome() const { return m_outcome; }
+
 private:
+    key_table_list      m_tables;
     cstring             m_sequence;
+#ifdef USE_TRIE
+    // TODO:  Need one m_node per key_table in m_tables.
     const key_trie*     m_node = nullptr;
+#endif
     const binding_target* m_target = nullptr;
+    dispatch_outcome    m_outcome = dispatch_outcome::miss;
 };
 
 }
--- tib/tib_bindings.cpp
+++ tib/tib_bindings.cpp
@@ -12,6 +12,7 @@
 
 namespace tib {
 
+#ifdef USE_TRIE
 struct key_trie;
 
 struct key_trie_entry
@@ -27,10 +28,13 @@ struct key_trie
 {
     key_trie_entry      entries[256];
 };
+#endif
 
 key_table::~key_table()
 {
+#ifdef USE_TRIE
     free_trie();
+#endif
 }
 
 void key_table::add(key_binding&& binding)
@@ -46,7 +50,9 @@ void key_table::add(key_binding&& binding)
     else
         m_bindings.insert(found, std::move(binding));
 
+#ifdef USE_TRIE
     free_trie(); // Regenerate on demand.
+#endif
 }
 
 void key_table::remove(const cstring& sequence)
@@ -60,15 +66,20 @@ void key_table::remove(const cstring& sequence)
     if (found != m_bindings.end() && found->sequence == sequence)
         m_bindings.erase(found);
 
+#ifdef USE_TRIE
     free_trie(); // Regenerate on demand.
+#endif
 }
 
 void key_table::clear()
 {
+#ifdef USE_TRIE
     free_trie();
+#endif
     m_bindings.clear();
 }
 
+#ifdef USE_TRIE
 const key_trie* key_table::get_trie()
 {
     if (!m_trie)
@@ -128,18 +139,32 @@ void key_table::free_trie()
         m_trie = nullptr;
     }
 }
+#endif
+
+void dispatcher::init(const key_table_list& tables)
+{
+    // Copy the key_table_list.  This isolates the dispatcher from changes to
+    // the caller's key_table_list.  Compromise:  however, it does not isolate
+    // from changes to a given key_table in the list.
+    m_tables = tables;
+    reset();
+}
 
 void dispatcher::reset()
 {
     m_sequence.clear();
+#ifdef USE_TRIE
     // TODO:  Where does it get the trie from?
     // TODO:  How to handle multiple tries...?
     m_node = nullptr;
+#endif
     m_target = nullptr;
+    m_outcome = dispatch_outcome::miss;
 }
 
 dispatch_outcome dispatcher::step(char c)
 {
+#ifdef USE_TRIE
     // If the new character doesn't match any existing key binding, then
     // discard any sequence so far (as many *nix input drivers seem to do).
     if (!m_node)
@@ -168,6 +193,48 @@ miss:
         goto miss;
 
     return dispatch_outcome::match;
+#else
+    if (m_outcome != dispatch_outcome::more)
+        reset();
+
+    m_sequence.append(&c, 1);
+
+    // Search they key tables in priority order (later entries overlay earlier
+    // entries) looking for an exact match or a prefix match.
+    bool is_prefix = false;
+    for (auto table = m_tables.rbegin(); table != m_tables.rend(); ++table)
+    {
+        const auto& bindings = (*table)->m_bindings;
+        const auto found = std::lower_bound(bindings.begin(), bindings.end(), m_sequence, [](const key_binding& candidate, const cstring& sequence) {
+            const size_t common_length = min(candidate.sequence.length(), sequence.length());
+            const int comparison = memcmp(candidate.sequence.c_str(), sequence.c_str(), common_length);
+            return comparison < 0 || (comparison == 0 && candidate.sequence.length() < sequence.length());
+        });
+
+        if (found != bindings.end() &&
+            found->sequence.length() >= m_sequence.length() &&
+            memcmp(found->sequence.c_str(), m_sequence.c_str(), m_sequence.length()) == 0)
+        {
+            if (found->sequence.length() == m_sequence.length())
+            {
+                m_target = &found->target;
+                m_outcome = dispatch_outcome::match;
+                return m_outcome;
+            }
+            is_prefix = true;
+        }
+    }
+
+    if (is_prefix)
+    {
+        m_outcome = dispatch_outcome::more;
+        return m_outcome;
+    }
+
+    m_sequence.set(&c, 1);
+    m_outcome = dispatch_outcome::miss;
+    return m_outcome;
+#endif
 }
 
 } // namespace tib
